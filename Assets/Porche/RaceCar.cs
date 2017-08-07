1039fbd7fa243b32ce3bb29ead532de7cefd054c
Patrick <pheney@gmail.com>
2017-08-06 22:12:20 -0500

    Whitelist test
    Start with this to ignore everything by default
    /*
    
    Then use this to make exceptions
    !.gitignore
    !Assets/
    
    Then for each folder, use this again to block everything
    Assets/*
    
    Then this to make exceptions
    !Assets/Porche/
    Assets/Porche/*
    !Assets/Porche/*.js
    !Assets/Porche/*.cs

---
+++ Assets/Porche/RaceCar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaceCar : MonoBehaviour {
+
+	public	float	speed	=	30;
+	public	float	turn	=	90;	
+
+	private Transform tr;
+
+	// Use this for initialization
+	void Start () {
+		tr = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float v = Input.GetAxis("Vertical");
+		float h = Input.GetAxis("Horizontal");
+
+		if (v != 0) {
+			tr.Translate(v * Vector3.forward * speed * Time.deltaTime);
+		}
+
+		if (h != 0) {
+			tr.Rotate(0, h * turn * Time.deltaTime, 0);
+		}
+
+	}
+}
+
+// test git whitelist
